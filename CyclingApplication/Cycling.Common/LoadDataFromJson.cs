81b284ff751ef8c7f74f14715ae8ebf00e6a1fb8
shopov91 <shopov91@gmail.com>
2017-06-05 16:46:37 +0300

    started implementig models

--- CyclingApplication/Cycling.Common/LoadDataFromJson.cs
+++ CyclingApplication/Cycling.Common/LoadDataFromJson.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,15 @@ namespace Cycling.Common
 {
     public class LoadDataFromJson
     {
+        static void Main(string[] args)
+        {
+            var jsonObj = JsonConvert.DeserializeObject("");
 
+            StreamReader re = new StreamReader("../../JsonData/CyclistsData.json");
+            var sss = JsonConvert.SerializeObject("../../ JsonData/CyclistsData.json");
+            Console.WriteLine(sss);
+           var json = JObject.Parse(sss);
+            //Console.WriteLine(parsedData[);
+        }
     }
 }
---
+++ CyclingApplication/Cycling.Models/Cyclist.cs
@@ -0,0 +1,21 @@
+﻿namespace Cycling.Models
+{
+    public class Cyclist
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public int TourDeFranceWins { get; set; }
+
+        public int GiroDItaliaWins { get; set; }
+
+        public int VueltaEspanaWins { get; set; }
+
+        public string CurrentTeam { get; set; }
+    }
+}
