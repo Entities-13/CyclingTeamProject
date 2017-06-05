f38fd5234dc578637dff47d780fe4c6daf157a78
shopov91 <shopov91@gmail.com>
2017-06-05 09:26:21 +0300

    started implementing json data loader

--- CyclingApplication/Cycling.Common/LoadDataFromJson.cs
+++ CyclingApplication/Cycling.Common/LoadDataFromJson.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cycling.Models
+namespace Cycling.Common
 {
-    public class Class1
+    public class LoadDataFromJson
     {
+
     }
 }
