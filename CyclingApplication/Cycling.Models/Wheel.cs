3b837941ce5d5c4ce0200ad87f84ea4951e7b34c
shopov91 <shopov91@gmail.com>
2017-06-07 18:14:32 +0300

    added wheels and tires models

---
+++ CyclingApplication/Cycling.Models/Wheel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cycling.Models
+{
+    public class Wheel
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Size { get; set; }
+
+        public virtual Tire Tire { get; set; }
+    }
+}
