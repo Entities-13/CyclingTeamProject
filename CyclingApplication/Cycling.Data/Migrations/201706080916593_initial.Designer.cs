fc2b0b872dfbc98ff52e1905eea819d17b9807d9
molivpopov <mitkop@mail.bg>
2017-06-08 12:31:11 +0300

    make new init migration
    Ванка, лепсваха някои миграции и беше трудно да се създаде нова база
    правилно, затова няколко промени ги обединих. Ако имаш друга идея можем
    да върнем това commit-vane

--- CyclingApplication/Cycling.Data/Migrations/201706080916593_initial.Designer.cs
+++ CyclingApplication/Cycling.Data/Migrations/201706080916593_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Cycling.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class restrictionsadded : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(restrictionsadded));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201706071532160_restrictions-added"; }
+            get { return "201706080916593_initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ CyclingApplication/Cycling.Data/Migrations/201706080916593_initial.cs
@@ -0,0 +1,93 @@
+namespace Cycling.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Bicycles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(nullable: false, maxLength: 40),
+                        Model = c.String(nullable: false, maxLength: 40),
+                        Wheel_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Wheels", t => t.Wheel_Id)
+                .Index(t => t.Wheel_Id);
+            
+            CreateTable(
+                "dbo.Cyclists",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 40),
+                        LastName = c.String(nullable: false, maxLength: 40),
+                        Age = c.Int(nullable: false),
+                        TourDeFranceWins = c.Int(nullable: false),
+                        GiroDItaliaWins = c.Int(nullable: false),
+                        VueltaEspanaWins = c.Int(nullable: false),
+                        CurrentTeam = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Wheels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(nullable: false, maxLength: 40),
+                        Size = c.Int(nullable: false),
+                        Tire_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Tires", t => t.Tire_Id)
+                .Index(t => t.Tire_Id);
+            
+            CreateTable(
+                "dbo.Tires",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(nullable: false, maxLength: 40),
+                        Size = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CyclistBicycles",
+                c => new
+                    {
+                        Cyclist_Id = c.Int(nullable: false),
+                        Bicycle_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Cyclist_Id, t.Bicycle_Id })
+                .ForeignKey("dbo.Cyclists", t => t.Cyclist_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Bicycles", t => t.Bicycle_Id, cascadeDelete: true)
+                .Index(t => t.Cyclist_Id)
+                .Index(t => t.Bicycle_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Bicycles", "Wheel_Id", "dbo.Wheels");
+            DropForeignKey("dbo.Wheels", "Tire_Id", "dbo.Tires");
+            DropForeignKey("dbo.CyclistBicycles", "Bicycle_Id", "dbo.Bicycles");
+            DropForeignKey("dbo.CyclistBicycles", "Cyclist_Id", "dbo.Cyclists");
+            DropIndex("dbo.CyclistBicycles", new[] { "Bicycle_Id" });
+            DropIndex("dbo.CyclistBicycles", new[] { "Cyclist_Id" });
+            DropIndex("dbo.Wheels", new[] { "Tire_Id" });
+            DropIndex("dbo.Bicycles", new[] { "Wheel_Id" });
+            DropTable("dbo.CyclistBicycles");
+            DropTable("dbo.Tires");
+            DropTable("dbo.Wheels");
+            DropTable("dbo.Cyclists");
+            DropTable("dbo.Bicycles");
+        }
+    }
+}
