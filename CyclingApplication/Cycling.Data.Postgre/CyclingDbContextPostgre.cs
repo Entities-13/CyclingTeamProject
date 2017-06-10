446636702616d2111a64c78a4876b9a24fb76c2b
shopov91 <shopov91@gmail.com>
2017-06-10 19:57:37 +0300

    started implementing dbContext and postgre models

--- CyclingApplication/Cycling.Data.Postgre/CyclingDbContextPostgre.cs
+++ CyclingApplication/Cycling.Data.Postgre/CyclingDbContextPostgre.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cycling.Data.Postgre
 {
-    public class CyclingDbContextPostgre
+    public class CyclingDbContextPostgre : DbContext
     {
+        public CyclingDbContextPostgre()
+            : base("CyclingPostgresDB")
+        {
+        }
+
+
     }
 }
