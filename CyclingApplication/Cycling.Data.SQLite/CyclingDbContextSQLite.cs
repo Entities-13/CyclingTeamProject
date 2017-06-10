78578e717fce8f6a188cf6dee366d13b2874ee3e
shopov91 <shopov91@gmail.com>
2017-06-10 23:18:43 +0300

    adding sqlLite to project

---
+++ CyclingApplication/Cycling.Data.SQLite/CyclingDbContextSQLite.cs
@@ -0,0 +1,17 @@
+﻿using Cycling.Models.SQLite;
+using System.Data.Entity;
+
+namespace Cycling.Data.SQLite
+{
+    public class CyclingDbContextSQLite : DbContext
+    {
+        public CyclingDbContextSQLite()
+            : base("CyclingSqliteDB")
+        {
+        }
+
+        public virtual IDbSet<CyclingInstructor> CyclingInstructors { get; set; }
+
+        public virtual IDbSet<CyclingDestination> CyclingDestination { get; set; }
+    }
+}
