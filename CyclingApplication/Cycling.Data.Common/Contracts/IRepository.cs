de9cc472a32ca711210d84c8238f27864e35a7a1
shopov91 <shopov91@gmail.com>
2017-06-11 18:01:21 +0300

    added main methods

--- CyclingApplication/Cycling.Data.Common/Contracts/IRepository.cs
+++ CyclingApplication/Cycling.Data.Common/Contracts/IRepository.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Cycling.Data.Common.Contracts
 {
-   public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
     }
 }
