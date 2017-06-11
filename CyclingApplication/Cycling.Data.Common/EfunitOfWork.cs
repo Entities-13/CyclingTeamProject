61e0ac4592c42eaccbb8f2568271b631d75892f6
shopov91 <shopov91@gmail.com>
2017-06-11 18:45:47 +0300

    ef unit of work done

---
+++ CyclingApplication/Cycling.Data.Common/EfunitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Cycling.Data.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycling.Data.Common
+{
+    public class EfunitOfWork : IUnitOfWork, IDisposable
+    {
+        private DbContext dbContext;
+
+        public EfunitOfWork(DbContext dbcontext)
+        {
+            this.dbContext = dbcontext;
+        }
+
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
