824a2954aceac1b9a0b0a395bc5d62a2b89faad4
shopov91 <shopov91@gmail.com>
2017-06-12 23:26:05 +0300

    CreateCyclist methods tested

---
+++ CyclingApplication/Cycling.App.Tests/DataProviders.Tests/CreateCyclistTest/CreateMethodsTests.cs
@@ -0,0 +1,44 @@
+﻿using Cycling.Web.Contracts;
+using Cycling.Web.DataProviders;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycling.App.Tests.DataProviders.Tests.CreateCyclistTest
+{
+    [TestFixture]
+    public class CreateMethodsTests
+    {
+        [Test]
+        public void CreateOneMethod_WhenPassedValuesAreValid_ShouldCallCorrectly()
+        {
+            // Arrange
+            var createCyclistMock = new Mock<ICreateCyclist>();
+
+            // Act
+            createCyclistMock.Object.CreateOne();
+            createCyclistMock.Object.CreateOne();
+
+            // Assert
+            createCyclistMock.Verify(x => x.CreateOne(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void CreateManyMethod_WhenPassedValuesAreValid_ShouldCallCorrectly()
+        {
+            // Arrange
+            var createCyclistMock = new Mock<ICreateCyclist>();
+
+            // Act
+            createCyclistMock.Object.CreateMany();
+            createCyclistMock.Object.CreateMany();
+            
+            // Assert
+            createCyclistMock.Verify(x => x.CreateMany(), Times.Exactly(2));
+        }
+    }
+}
--- CyclingApplication/Cycling.App.Tests/DataProviders.Tests/CreateCyclistTest/PropertiesTests.cs
+++ CyclingApplication/Cycling.App.Tests/DataProviders.Tests/CreateCyclistTest/PropertiesTests.cs
@@ -67,5 +67,73 @@ namespace Cycling.App.Tests.DataProviders.Tests.CreateCyclistTest
             // Arrange& Act & Assert
             Assert.Throws<NullReferenceException>(() => new CreateCyclist("Dragan", invalidLastName, 13, 0, 1, 2, "Segafredo"));
         }
+
+        [Test]
+        public void PropertyAge_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var createCyclist = new CreateCyclist("Dragan", "Draganov", 30, 0, 1, 2, "Segafredo");
+
+            // Act & Assert
+            Assert.AreEqual(30, createCyclist.Age);
+        }
+
+        [Test]
+        public void PropertyTourDeFranceWins_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var createCyclist = new CreateCyclist("Dragan", "Draganov", 30, 3, 0, 0, "Segafredo");
+
+            // Act & Assert
+            Assert.AreEqual(3, createCyclist.TourDeFranceWins);
+        }
+
+        [Test]
+        public void PropertyGiroDItaliaWins_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var createCyclist = new CreateCyclist("Dragan", "Draganov", 30, 0, 3, 0, "Segafredo");
+
+            // Act & Assert
+            Assert.AreEqual(3, createCyclist.GiroDItaliaWins);
+        }
+
+        [Test]
+        public void PropertyVueltaWins_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var createCyclist = new CreateCyclist("Dragan", "Draganov", 30, 0, 0, 3, "Segafredo");
+
+            // Act & Assert
+            Assert.AreEqual(3, createCyclist.VueltaWins);
+        }
+
+        [Test]
+        public void PropertyCurrentTeam_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var createCyclist = new CreateCyclist("Dragan", "Draganov", 30, 0, 0, 3, "Segafredo");
+
+            // Act & Assert
+            StringAssert.Contains("Segafredo", createCyclist.CurrentTeam);
+        }
+
+        [Test]
+        public void PropertyCurrentTeam_WhenPassedValueIsLongerThan40Chars_ShouldThrowIndexOutOfRangeException()
+        {
+            // Arrange
+            var invalidCurrentTeam = "this name is way to long, so it is not goinig to be a valid name and a exception should be trown!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+
+            // Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new CreateCyclist("Dragan", "Draganov", 13, 0, 1, 2, invalidCurrentTeam));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void PropertyCurrentTeam_WhenPassedValueIsNullOrEmpty_ShouldThrowNullReferenceExceptionException(string invalidCurrentTeam)
+        {
+            // Arrange& Act & Assert
+            Assert.Throws<NullReferenceException>(() => new CreateCyclist("Dragan", "Draganov", 13, 0, 1, 2, invalidCurrentTeam));
+        }
     }
 }
