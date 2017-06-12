f745a5baaa39d4c22572bdaa73a172add685711a
shopov91 <shopov91@gmail.com>
2017-06-12 14:24:13 +0300

    postgre button and data added

--- CyclingApplication/Cycling.Web/SQLiteAndPostgre.aspx
+++ CyclingApplication/Cycling.Web/SQLiteAndPostgre.aspx
@@ -5,5 +5,6 @@
      <div class="well well-sm">
         <br />
         <asp:Button ID="ButtonFillSQLite" runat="server" Text="Fill SQLite With Data" CssClass="btn btn-warning" OnClick="ButtonFillSQLite_Click" />
-    </div>
+        <asp:Button ID="ButtonFillPostgre" runat="server" Text="Fill PostgreSQL With Data" CssClass="btn btn-warning" OnClick="ButtonFillPostgre_Click" />    
+     </div>
 </asp:Content>
--- CyclingApplication/Cycling.Web/SQLiteAndPostgre.aspx.designer.cs
+++ CyclingApplication/Cycling.Web/SQLiteAndPostgre.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace Cycling.Web {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button ButtonFillSQLite;
+        
+        /// <summary>
+        /// ButtonFillPostgre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonFillPostgre;
     }
 }
