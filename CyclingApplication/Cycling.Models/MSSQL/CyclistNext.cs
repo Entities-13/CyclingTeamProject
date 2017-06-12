6eda4a7f628f77c873543c7c051700aa3fc9b2ac
molivpopov <mitkop@mail.bg>
2017-06-12 13:25:34 +0300

    small constraint changes

--- CyclingApplication/Cycling.Models/MSSQL/CyclistNext.cs
+++ CyclingApplication/Cycling.Models/MSSQL/CyclistNext.cs
@@ -11,24 +11,24 @@ namespace Cycling.Models.MSSQL
 {
     public class CyclistNext
     {
-        private const string invalidName = "Names must be more than 3 and less than 40 sumbols, only Latyn symbols, not separated by any symbol";
-        private const string invalidNationality = "Nationality must be more than 3 and less than 40 sumbols, only latin symbol, separeted by comma";
+        private const string invalidNameMessage = "Names must be more than 3 and less than 40 sumbols, only Latyn symbols, not separated by any symbol";
+        private const string invalidNationalityMessage = "Nationality must be more than 3 and less than 40 sumbols, only latin symbols, separeted by comma";
 
 
         public int Id { get; set; }
 
         [MaxLength(40)]
-        [RegularExpression("^[A-Za-z]{3,40}$", ErrorMessage = invalidName)]
+        [RegularExpression("^[A-Za-z]{3,40}$", ErrorMessage = invalidNameMessage)]
         public string FirstName { get; set; }
 
         [MaxLength(40)]
-        [RegularExpression("^[A-Za-z]{3,40}$", ErrorMessage = invalidName)]
+        [RegularExpression("^[A-Za-z]{3,40}$", ErrorMessage = invalidNameMessage)]
         public string LastName { get; set; }
 
         public DateTime BirthDay { get; set; }
 
         [MaxLength(40)]
-        [RegularExpression("^[A-Za-z ]{3,40}$", ErrorMessage = invalidNationality)]
+        [RegularExpression("^[A-Za-z ]{3,40}$", ErrorMessage = invalidNationalityMessage)]
         public string Nationality { get; set; }
 
         public virtual ICollection<TourDeFrance> TF_Id { get; set; }
