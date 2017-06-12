fe1688e55a27d7665749c06e3ede99f789d2642a
shopov91 <shopov91@gmail.com>
2017-06-12 23:36:39 +0300

    remove cyclist test added

---
+++ CyclingApplication/Cycling.App.Tests/DataProviders.Tests/RemoveCyclistTests/PropertiesTests.cs
@@ -0,0 +1,42 @@
+﻿using Cycling.Web.DataProviders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycling.App.Tests.DataProviders.Tests.RemoveCyclistTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [Test]
+        public void PropertyFirstName_WhenPassedValueIsValid_ShouldSetCorrectly()
+        {
+            // Arrange
+            var removeCyclist = new RemoveCyclist("Dragan", "Draganov");
+
+            // Act & Assert
+            StringAssert.Contains("Dragan", removeCyclist.FirstName);
+        }
+
+        [Test]
+        public void PropertyFirstName_WhenPassedValueIsLongerThan40Chars_ShouldThrowIndexOutOfRangeException()
+        {
+            // Arrange
+            var invalidFirstName = "this name is way to long, so it is not goinig to be a valid name and a exception should be trown!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+
+            // Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new RemoveCyclist(invalidFirstName, "Draganov"));
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void PropertyFirstName_WhenPassedValueIsNullOrEmpty_ShouldThrowNullReferenceExceptionException(string invalidFirstName)
+        {
+            // Arrange& Act & Assert
+            Assert.Throws<NullReferenceException>(() => new RemoveCyclist(invalidFirstName, "Draganov"));
+        }
+    }
+}
